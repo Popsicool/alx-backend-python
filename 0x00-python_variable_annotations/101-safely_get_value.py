25d2397f33116d34f9fa8b2cc0095c9196644244
Popsicool <akinolasamson1234@gmail.com>
2023-04-27 12:31:29 +0100

    advance taks

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+Python - Variable Annotations
+'''
+
+
+from typing import Mapping, TypeVar, Any, Union, Optional
+
+
+T = TypeVar("T")
+
+
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Optional[T] = None) -> Union[Any, T]:
+    if key in dct:
+        return dct[key]
+    else:
+        return default
