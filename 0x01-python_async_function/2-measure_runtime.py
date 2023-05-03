495baa0e8f47e225f6ddacff1ca3ad7cddc37c8d
Popsicool <akinolasamson1234@gmail.com>
2023-05-03 07:00:04 +0100

    hot fix

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -13,6 +13,9 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
+    '''
+    Measure the runtime
+    '''
     start = perf_counter()
     asyncio.run(wait_n(n, max_delay))
     count = perf_counter() - start
