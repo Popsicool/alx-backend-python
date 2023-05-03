a16529ed205299228730980a8c9dcdcccb48ed4e
Popsicool <akinolasamson1234@gmail.com>
2023-05-03 07:33:07 +0100

    task 0 - 2

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''
+Run time for four parallel comprehensions
+'''
+
+
+import asyncio
+import time
+
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    start = time.perf_counter()
+    task = []
+    for _ in range(4):
+        task.append(async_comprehension())
+    await asyncio.gather(*task)
+    end = time.perf_counter() - start
+    return end
